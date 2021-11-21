1652479ba253be23a79a1fee699b952b6d94f46c
LeTrongNhan2511 <trongnhanl2511@gmail.com>
2021-11-22 02:17:35 +0700

    html

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import hello_world
+from . import views
 
 urlpatterns = [
-    path("hello_world/", hello_world, name="hello_world")
+    path("", views.post_list, name="post_list")
 ]
